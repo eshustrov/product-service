c91d8a7eb483b4fc2bfd123ac8fb9624c93c4f92
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-05-20 22:31:10 +0300

    service configuration is moved to a separate module

--- src/main/java/net/nowhere/service/product/environment/ProductServiceConfig.java
+++ src/main/java/net/nowhere/service/product/environment/ProductServiceConfig.java
@@ -3,13 +3,7 @@ package net.nowhere.service.product.environment;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
-import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.guice.JerseyServletModule;
-import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
-import net.nowhere.service.product.service.ProductService;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ProductServiceConfig extends GuiceServletContextListener {
     @Override
@@ -17,11 +11,7 @@ public class ProductServiceConfig extends GuiceServletContextListener {
         return Guice.createInjector(new JerseyServletModule() {
             @Override
             protected void configureServlets() {
-                bind(ProductService.class).asEagerSingleton();
-
-                final Map<String, String> featureMap = new HashMap<>();
-                featureMap.put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE.toString());
-                serve("/*").with(GuiceContainer.class, featureMap);
+                install(new ServiceModule());
             }
         });
     }
---
+++ src/main/java/net/nowhere/service/product/environment/ServiceModule.java
@@ -0,0 +1,20 @@
+package net.nowhere.service.product.environment;
+
+import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.api.json.JSONConfiguration;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import net.nowhere.service.product.service.ProductService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServiceModule extends ServletModule {
+    @Override
+    protected void configureServlets() {
+        bind(ProductService.class).asEagerSingleton();
+
+        final Map<String, String> featureMap = new HashMap<>();
+        featureMap.put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE.toString());
+        serve("/*").with(GuiceContainer.class, featureMap);
+    }
+}
