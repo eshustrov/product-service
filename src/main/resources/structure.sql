f2f160eeea4a63a7ae7e3cb675b9c0a349950d15
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-05-23 03:15:47 +0300

    more test products are added to database

--- src/main/resources/structure.sql
+++ src/main/resources/structure.sql
@@ -3,4 +3,5 @@ CREATE TABLE products (
        name VARCHAR(30) NOT NULL
 );
 
-INSERT INTO products (id, name) VALUES (1, 'product');
+INSERT INTO products (id, name) VALUES (1, 'apple');
+INSERT INTO products (id, name) VALUES (2, 'pear');
